864b4ea070c1c6e01dfa85253b689cb46d63f537
prateekjain0530 <52654340+prateekjain0530@users.noreply.github.com>
2020-04-28 05:54:16 -0700

    Add files via upload

---
+++ flaskapp/main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+app.run(host="localhost",port=80, debug=True)
